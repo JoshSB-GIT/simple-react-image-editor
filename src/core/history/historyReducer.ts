c613472805cd2f53369a9fd038bc4b1acd041fb2
Joseph <sierrajoseph8023@gmail.com>
2026-07-29 08:50:06 -0500

    feat(history): implement redo functionality in history reducer

--- src/core/history/historyReducer.ts
+++ src/core/history/historyReducer.ts
@@ -1,5 +1,6 @@
 import type { EditorHistory } from "./types";
 import type { EditorAction } from "../../state/editorActions";
+import { editorReducer } from "../../state/editorReducer";
 
 function setUndo(state: EditorHistory): EditorHistory {
   if (state.past.length > 0) {
@@ -13,9 +14,26 @@ function setUndo(state: EditorHistory): EditorHistory {
   return state;
 }
 
+function setRedo(state: EditorHistory): EditorHistory {
+  if (state.future.length > 0) {
+    return {
+      past: [...state.past, state.present],
+      present: state.future[state.future.length - 1],
+      future: state.future.slice(0, state.future.length - 1),
+    };
+  }
+
+  return state;
+}
+
 export function historyReducer(state: EditorHistory, action: EditorAction) {
   switch (action.type) {
     case "UNDO":
       return setUndo(state);
+
+    case "REDO":
+      return setRedo(state);
   }
+
+  editorReducer(state, action);
 }
